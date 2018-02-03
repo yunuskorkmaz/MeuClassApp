0741811c02316296b9c62483dffa1f92a6b4da81
furkanbayram2 <sefikfurkanbayram@gmail.com>
2018-02-04 00:51:40 +0300

    Admin panelinde ders, personel, üye ekleme ve listeleme sayfaları oluşturuldu.

---
+++ MeuClass/Areas/Admin/PersonnelController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeuClass.Areas.Admin
+{
+    public class PersonnelController : Controller
+    {
+        // GET: Admin/Personnel
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddPersonnel(string number)
+        {
+            ViewBag.Number = number;
+            return View();
+        }
+        public ActionResult ViewPersonnel(string number)
+        {
+            ViewBag.Number = number;
+            return View();
+        }
+    }
+}
