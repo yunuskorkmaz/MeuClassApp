a2f21a3e239a11bbef0edeecdf8c3ac51c01e826
ufukbasyigit <ubasyigitt@gmail.com>
2018-02-04 05:32:05 +0300

    Admin paneli için ders ekleme özelliği eklendi

--- MeuClass/Areas/Admin/Controllers/AdminLessonController.cs
+++ MeuClass/Areas/Admin/Controllers/AdminLessonController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MeuClass.Business.Repository;
+using MeuClass.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,15 +15,48 @@ namespace MeuClass.Areas.Admin.Controllers
         {
             return View();
         }
-        public ActionResult AddLesson(string number)
-        {
-            ViewBag.Number = number;
-            return View();
-        }
         public ActionResult ViewLesson(string number)
         {
             ViewBag.Number = number;
             return View();
+        }
+        public ActionResult AddLesson()
+        {
+            if (Session["user_id"] == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            else
+            {
+                ViewBag.Title = "Ders Ekle | Admin Paneli";
+                ClassAppContext context = new ClassAppContext();
+                IEnumerable<User> users = new List<User>();
+                users = context.User.Where(x => x.UserTypeID == 2);
+                ViewBag.users = users;
+                return View();
+            }
+        }
+        public ActionResult AddLessonRegister(FormCollection form)
+        {
+            var lesson = new Lesson()
+            {
+                LessonName = form.Get("lessonName"),
+                LessonCode = form.Get("lessonCode"),
+                RecordDate = DateTime.Now
+            };
+
+            var insert = LessonRepository.Instance.Add(lesson);
+
+            if (insert.Success == false)
+            {
+                TempData["error"] = insert.Message;
+                return RedirectToAction("Index", "AdminLesson");
+
+            }
+            else
+            {
+                return RedirectToAction("AddLesson", "AdminLesson");
+            }
         }
     }
 }
