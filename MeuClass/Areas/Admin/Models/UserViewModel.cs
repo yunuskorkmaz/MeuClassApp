5fb353e4cfeb15567f4edc2385c80ab6969b3fc2
ufukbasyigit <ubasyigitt@gmail.com>
2018-02-24 17:54:15 +0300

    Üye listeleme ve editleme sayfaları kodlanıyor. Çalışmalar devam ediyor henüz tamamlanmadı...

---
+++ MeuClass/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MeuClass.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeuClass.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public User User { get; set; }
+        public List<UserType> UserTypes { get; set; }
+    }
+}
